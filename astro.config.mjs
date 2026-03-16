b9c7846226b8839b1372eb6f144244c365ff9b54
Oren Hollander <oren.hollander@gmail.com>
2026-03-16 15:21:50 +0200

    UI tweaks: larger signature, remove subtitle text, disable dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://oren-hollander.github.io',
   base: process.env.BUILD_BASE || '/',
+  devToolbar: { enabled: false },
 });
